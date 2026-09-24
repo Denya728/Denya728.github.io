fdbb12dbcdad92f30a23650ab3d3e0d408d41e49
Denya728 <denicake728@gmail.com>
2026-09-24 13:45:04 -0600

    Restore denya2/app-v110-profile-final.js to pre-profile-debug version

--- denya2/app-v110-profile-final.js
+++ denya2/app-v110-profile-final.js
@@ -246,4 +246,6 @@
   views.profile=()=>render('company');
   const pb=document.querySelector('.nav button[data-view="profile"]');if(pb)pb.onclick=()=>render('company');
   document.querySelectorAll('.nav button[data-view="plan"],.nav button[data-view="subscription"]').forEach(x=>x.remove());
-  window.DENYAProfileV110={render}; })();
+  window.DENYAProfileV110={render};
+  render('company');
+})();
