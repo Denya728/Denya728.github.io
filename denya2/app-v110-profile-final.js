84768c26aa7e37e031760e5f147ea4e78eed10ba
Denya728 <denicake728@gmail.com>
2026-09-24 13:38:32 -0600

    Prevent legacy profile script from rendering on boot

--- denya2/app-v110-profile-final.js
+++ denya2/app-v110-profile-final.js
@@ -246,6 +246,4 @@
   views.profile=()=>render('company');
   const pb=document.querySelector('.nav button[data-view="profile"]');if(pb)pb.onclick=()=>render('company');
   document.querySelectorAll('.nav button[data-view="plan"],.nav button[data-view="subscription"]').forEach(x=>x.remove());
-  window.DENYAProfileV110={render};
-  render('company');
-})();
+  window.DENYAProfileV110={render}; })();
